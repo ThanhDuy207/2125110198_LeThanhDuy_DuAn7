7e72941f4f4d75635e9351cc428ffd318733d529
ThanhDuy207 <duyle.261107@gmail.com>
2026-07-01 13:35:59 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,20 @@ int main()
         cout << "1. Tao me cung va choi\n";
         cout << "2. Thoat\n";
         cout << "Lua chon: ";
+        int level;
+
+        cout << "Chon do kho:\n";
+        cout << "1. De\n";
+        cout << "2. Trung binh\n";
+        cout << "3. Kho\n";
+        cin >> level;
+
+        switch (level)
+        {
+        case 1: n = 11; break;
+        case 2: n = 15; break;
+        case 3: n = 21; break;
+        }
         cin >> lc;
 
         switch (lc)
