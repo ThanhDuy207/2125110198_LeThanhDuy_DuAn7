1f470a0e49fac482b64e243bfa8d85d39f46d3da
ThanhDuy207 <duyle.261107@gmail.com>
2026-07-01 13:18:06 +0700

    khaibao.h

---
+++ khaibao.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <conio.h>
+using namespace std;
+
+extern vector<string> a;
+extern int n, x, y;
+
+void taoMeCung();
+void hienThi();
+void choi();
